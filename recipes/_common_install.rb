a6ab61a53356ba0c13863424d0df7dba01297639
crobertsdrc <55412117+crobertsdrc@users.noreply.github.com>
2021-01-04 19:10:36 -0500

    Update _common_install.rb

--- recipes/_common_install.rb
+++ recipes/_common_install.rb
@@ -1,5 +1,17 @@
 case node['platform_family']
 when 'debian'
   # needed for uwsgi starting with version 1.3
-  package "libssl0.9.8"
+  #  == 'ubuntu' && node['platform_version'] == '18.04'
+  if node['platform']
+    case node['platform_version']
+    when '18.04'
+        package 'libssl1.1'
+    when '16.04'
+        package "libssl1.0.0"
+    else
+        package "libssl0.9.8"
+    end
+  end
 end
+
+include_recipe 'build-essential::default'
